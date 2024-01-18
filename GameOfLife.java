1f8578fc60af5a82866e82b6863c4fba140eceff
eitammor <154350216+eitammor@users.noreply.github.com>
2024-01-18 12:24:51 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -189,18 +189,7 @@ public class GameOfLife {
         {
             for (int j = 1; j<cols-1; j++)
             {
-                /*
-                 *                 if (j==cols-2)
-                {
-                    System.out.print(arr[i][j]);
-                }
-                else
-                {}
-                 */
-
-                    System.out.print( "  "+arr[i][j]);
-                
-                
+              System.out.print( "  "+arr[i][j]);
             }
             System.out.println("");
         }
